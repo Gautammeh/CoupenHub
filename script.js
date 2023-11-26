6af25fb6067f4521252f012c912f6c5e6cf545f3
Gautammeh <gautammehta092@gmail.com>
2023-11-26 17:56:31 +0530

    new web

---
+++ script.js
@@ -0,0 +1,50 @@
+const leftArrow = document.getElementById("left-arrow")
+const rightArrow = document.getElementById("right-arrow")
+const coupen = document.getElementsByClassName("coupen-cards")
+
+leftArrow.addEventListener("click",()=>{
+    
+    for(let i = 0; i < coupen.length; i++){
+        coupen[i].style.transform = "translateX(-100px)"
+    }
+})
+
+rightArrow.addEventListener("click",()=>{
+    
+    for(let i = 0; i < coupen.length; i++){
+        coupen[i].style.transform = "translateX(0px)"
+    }
+})
+
+
+const hamburger = document.getElementById("hamburger");
+const dropdown = document.getElementById("dropdown");
+
+
+
+let val = 0
+hamburger.addEventListener("click",()=>{
+
+    if(val== 0){
+
+        
+    
+    dropdown.style.display = "block";
+    hamburger.removeAttribute;
+    hamburger.setAttribute("class","ri-close-fill");
+    val = 1;
+
+
+    }
+    else{
+    dropdown.style.display = "none";
+    hamburger.removeAttribute;
+    hamburger.setAttribute("class","ri-menu-line");
+    val = 0;
+
+    }
+    
+
+
+
+})
